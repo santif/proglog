4bb8fd9bf17851bb6a1375dff6221c0458b9e5f7
Santiago Fernandez <santif@gmail.com>
2021-06-15 07:03:32 -0300

    Add tests - chapter 3

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/santif/proglog
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.4.1 // indirect
+	github.com/golang/protobuf v1.4.1
 	github.com/gorilla/mux v1.8.0
-	github.com/stretchr/testify v1.7.0 // indirect
-	github.com/tysontate/gommap v0.0.0-20210506040252-ef38c88b18e1 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	github.com/stretchr/testify v1.7.0
+	github.com/tysontate/gommap v0.0.0-20210506040252-ef38c88b18e1
+	google.golang.org/protobuf v1.25.0
 )
--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -12,6 +12,7 @@ var (
 	write = []byte("hello world")
 	width = uint64(len(write)) + lenWidth
 )
+
 func TestStoreAppendRead(t *testing.T) {
 	f, err := ioutil.TempFile("", "store_append_read_test")
 	require.NoError(t, err)
@@ -62,4 +63,37 @@ func testReadAt(t *testing.T, s *store) {
 		require.Equal(t, int(size), n)
 		off += int64(n)
 	}
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	err = s.Close()
+	require.NoError(t, err)
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
 }
