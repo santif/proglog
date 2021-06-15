de1715eb63ea5f9fe664bfe47a2da049df222b17
Santiago Fernandez <santif@gmail.com>
2021-06-15 07:34:40 -0300

    Format code

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -36,9 +36,11 @@ func testAppendRead(t *testing.T, log *Log) {
 	append := &api.Record{
 		Value: []byte("hello world"),
 	}
+
 	off, err := log.Append(append)
 	require.NoError(t, err)
 	require.Equal(t, uint64(0), off)
+
 	read, err := log.Read(off)
 	require.NoError(t, err)
 	require.Equal(t, append.Value, read.Value)
@@ -54,22 +56,29 @@ func testInitExisting(t *testing.T, o *Log) {
 	append := &api.Record{
 		Value: []byte("hello world"),
 	}
+
 	for i := 0; i < 3; i++ {
 		_, err := o.Append(append)
 		require.NoError(t, err)
 	}
+
 	require.NoError(t, o.Close())
+
 	off, err := o.LowestOffset()
 	require.NoError(t, err)
 	require.Equal(t, uint64(0), off)
+
 	off, err = o.HighestOffset()
 	require.NoError(t, err)
 	require.Equal(t, uint64(2), off)
+
 	n, err := NewLog(o.Dir, o.Config)
 	require.NoError(t, err)
+
 	off, err = n.LowestOffset()
 	require.NoError(t, err)
 	require.Equal(t, uint64(0), off)
+
 	off, err = n.HighestOffset()
 	require.NoError(t, err)
 	require.Equal(t, uint64(2), off)
@@ -79,12 +88,15 @@ func testReader(t *testing.T, log *Log) {
 	append := &api.Record{
 		Value: []byte("hello world"),
 	}
+
 	off, err := log.Append(append)
 	require.NoError(t, err)
 	require.Equal(t, uint64(0), off)
+
 	reader := log.Reader()
 	b, err := ioutil.ReadAll(reader)
 	require.NoError(t, err)
+
 	read := &api.Record{}
 	err = proto.Unmarshal(b[lenWidth:], read)
 	require.NoError(t, err)
@@ -95,12 +107,15 @@ func testTruncate(t *testing.T, log *Log) {
 	append := &api.Record{
 		Value: []byte("hello world"),
 	}
+
 	for i := 0; i < 3; i++ {
 		_, err := log.Append(append)
 		require.NoError(t, err)
 	}
+
 	err := log.Truncate(1)
 	require.NoError(t, err)
+
 	_, err = log.Read(0)
 	require.Error(t, err)
 }
